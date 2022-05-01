378f6e3977c6a1c8feb95e3c381d53bd623af879
DarwingJuarez <100969487+DarwingJuarez@users.noreply.github.com>
2022-04-30 20:05:32 -0600

    Add files via upload

---
+++ Ejercicio 12.cpp
@@ -0,0 +1,16 @@
+//Construir un programa que despliegue los numeros del 20 al 30
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main(){
+
+	cout<<"NUMEROS DEL 20 AL 30\n";
+	for(int numero=20; numero<=30; numero++){
+		cout<<numero<<endl;
+	}
+	
+	
+	getch();
+	return 0;
+}
